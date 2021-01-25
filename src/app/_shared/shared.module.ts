fb836038c3e888ae18159a62dc534e787f28f810
Dimitrije Radosavljevic <dimitrijedimitrije6@gmail.com>
2021-01-25 19:40:33 +0100

    Add shared module

---
+++ src/app/_shared/shared.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
+import {ToastrModule} from 'ngx-toastr';
+import {RouterModule} from '@angular/router';
+import {NgxPaginationModule} from 'ngx-pagination';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    ToastrModule.forRoot({
+      timeOut: 2000,
+      positionClass: 'toast-top-right',
+    }),
+    NgxPaginationModule,
+    RouterModule,
+  ],
+  exports: [
+    CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    NgxPaginationModule,
+  ]
+})
+export class SharedModule { }
